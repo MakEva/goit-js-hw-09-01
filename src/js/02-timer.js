a3639bedc8770922748059109acdaabe4ff2b828
MakEva <sher.evikm@gmail.com>
2023-11-14 00:40:32 +0200

    2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,16 +1,21 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
+import Notiflix from 'notiflix';
 
 const refs = {
     inputEl: document.querySelector("#datetime-picker"),
     startBtn: document.querySelector("[data-start]"),
     timerEl: document.querySelector(".timer"),
+    daysEl: document.querySelector("[data-days]"),
+    hoursEl: document.querySelector("[data-hours]"),
+    minutesEl: document.querySelector("[data-minutes]"),
+    secondsEl: document.querySelector("[data-seconds]"),  
 };
+console.log(refs.hoursEl);
 
 refs.startBtn.disabled = true;
 let selectedDate = null;
-let currentDate = new Date();
-const timerId = null;
+let timerId = null;
 
 const options = {
   enableTime: true,
@@ -21,8 +26,8 @@ const options = {
   onClose(selectedDates) {
     console.log(selectedDates[0]);
     selectedDate = selectedDates[0];
-    if (selectedDate <= currentDate) {
-        alert("Please choose a date in the future");
+    if (selectedDate <= Date.now()) {
+      Notiflix.Notify.success("Please choose a date in the future");
         refs.startBtn.disabled = true;
       } else {
         refs.startBtn.disabled = false;
@@ -36,12 +41,15 @@ refs.startBtn.addEventListener("click", onStartBtnClick);
 
 function onStartBtnClick() {
  timerId = setInterval(() => {
-    const ms = selectedDate - currentDate;
-    // return ms;
-  }, 1000);
-  if (ms <= 0) {
+   const ms = selectedDate - Date.now();
+   
+   convertMs(ms);
+
+   if (ms <= 1000) {
     clearInterval(timerId);
   }
+  }, 1000);
+  
 }
 
 function convertMs(ms) {
@@ -54,11 +62,15 @@ function convertMs(ms) {
   const hours = Math.floor((ms % day) / hour);
   const minutes = Math.floor(((ms % day) % hour) / minute);
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-
+  
+  refs.daysEl.textContent = addLeadingZero(days);
+  refs.hoursEl.textContent = addLeadingZero(hours);
+  refs.minutesEl.textContent = addLeadingZero(minutes);
+  refs.secondsEl.textContent = addLeadingZero(seconds);
+  
   return { days, hours, minutes, seconds };
 }
 
-function addLeadingZero({ days, hours, minutes, seconds }) {
-
+function addLeadingZero(value) {
+  return value.toString().padStart(2, '0');
 }
-
