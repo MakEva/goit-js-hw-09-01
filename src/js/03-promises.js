9cb4dfd6e206a699fcc5ca75fc1e62766828b595
MakEva <sher.evikm@gmail.com>
2023-11-15 01:46:05 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -12,31 +12,31 @@ refs.formEl.addEventListener("submit", onSubmitForm);
 
 function onSubmitForm(evt) {
   evt.preventDefault();
-  let firstDelayValue = parseInt(refs.firstDelayEl.value);
-  let delayStepValue = parseInt(refs.delayStepEl.value);
-  let amountValue = parseInt(refs.amount.value);
+  const firstDelayValue = parseInt(refs.firstDelayEl.value);
+  const delayStepValue = parseInt(refs.delayStepEl.value);
+  const amountValue = parseInt(refs.amount.value);
  
   for (let i = 0; i < amountValue; i += 1) {
 
-    createPromise(i+1, firstDelayValue + i * delayStepValue)
-  .then(({ position, delay }) => {
-   Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
-  })
-  .catch(({ position, delay }) => {
-    Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
-  })
-  }  
-}
+    createPromise(i + 1, firstDelayValue + i * delayStepValue)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+  };  
+};
 
 function createPromise(position, delay) {
   return new Promise((resolve, reject) => {
-    setTimeout(() => {
     const shouldResolve = Math.random() > 0.3;
-        if (shouldResolve) {
+        setTimeout(() => {
+          if (shouldResolve) {
           resolve({ position, delay });
-        } else {
+          } else {
           reject({ position, delay });
-        }
+          }
           }, delay);
 })
 }
